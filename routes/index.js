36b924f3f8cd2cdf0ef937fc525a2faca52049fa
Somanyu <somanyu.03samal@gmail.com>
2022-03-27 08:54:52 +0530

    router for /login & /register listing

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET /register listing. */
+router.get('/register', function(req, res, next) {
+  res.render('register', { title: 'Library MS | Register' });
+});
+
+/* GET /login listing. */
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Library MS | Login' });
+});
+
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+/* GET /register listing. */
+router.get('/api/register', function(req, res, next) {
+  res.render('register', { title: 'Library MS | Register' });
 });
 
 module.exports = router;
