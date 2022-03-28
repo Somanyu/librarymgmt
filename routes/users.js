96d1c1ab992c5e439eb88012ae4c3b5809e443ce
Somanyu <somanyu.03samal@gmail.com>
2022-03-28 11:20:48 +0530

    Added registeration with validation

--- routes/users.js
+++ routes/users.js
@@ -1,23 +1,34 @@
 var express = require('express');
 const User = require('../model/User');
+const Joi = require('joi');
 var router = express.Router();
+const { registerValidation, loginValidation } = require('../validation/validation');
+
 
 /* POST /api/register listing. */
 router.post('/api/register',  async(req, res, next) => {
-  const user = new User({
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    email: req.body.email,
-    password: req.body.password
-  });
 
+  let firstName = req.body.firstName;
+  let lastName = req.body.lastName;
+  let email = req.body.email;
+  let password = req.body.password;
+
+  const { error } = registerValidation(req.body);
+  if(error) return res.status.send(error);
+
+  const user = new User({
+    firstName: firstName,
+    lastName: lastName,
+    email: email,
+    password: password
+  });
   try {
     const savedUser = await user.save();
-    res.send(savedUser);
-  } catch (error) {
-    next();
+    res.send('User saved in database');
+  } catch(err) {
+    console.log(err);
   }
-  
+
 });
 
 /* POST /api/login listing. */
