87f9e232d05d6c9c769671e9abaaa82f6588230f
Somanyu <somanyu.03samal@gmail.com>
2022-03-28 11:20:59 +0530

    validation of login and register

---
+++ validation/validation.js
@@ -0,0 +1,45 @@
+const Joi = require('joi');
+
+const registerValidation = (data) => {
+    const schema = Joi.object({
+        firstName: Joi.string()
+                .alphanum()
+                .min(4)
+                .required(),
+        lastName: Joi.string()
+                .alphanum()
+                .min(4)
+                .required(),
+        email: Joi.string()
+                .min(10)
+                .required()
+                .email(),
+        password: Joi.string()
+                .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$'))
+                .required()
+      });
+      return schema.validate(data);
+};
+
+const schemaLoginToken = Joi.object({
+    token: Joi.string()
+        .required(),
+});
+
+const schemaLogin = Joi.object({
+    email: Joi.string()
+        .min(10)
+        .required()
+        .email(),
+    password: Joi.string()
+        .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$'))
+        .required(),
+});
+
+const loginValidation = (data) => {
+    if(data.token) return schemaLoginToken(data)
+    return schemaLogin.validate(data);
+};
+
+module.exports.registerValidation = registerValidation;
+module.exports.loginValidation = loginValidation;
