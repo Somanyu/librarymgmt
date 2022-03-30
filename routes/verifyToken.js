83a2ee11360d0d81999ca17280ab0bbca727c56d
Somanyu <somanyu.03samal@gmail.com>
2022-03-30 13:02:36 +0530

    JWT token verification

--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -1,11 +1,11 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = function (req, res, next) {
-    const token = req.header('auth-token');
-    if(!token) return res.status(401).send('Access Denied');
+    const token = req.cookies.jwt;
+    if(!token) return res.status(401).redirect('/login');
 
     try {
-        const verified = jwt.verify(token, process.env.TOKEN_SECRET);
+        const verified = jwt.verify(token, process.env.JWT_SECRET);
         req.user = verified;
         next();
     } catch (error) {
