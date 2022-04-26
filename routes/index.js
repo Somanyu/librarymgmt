7a7f49ea83610d033ccd55f9710f9ae102b67cef
Somanyu <somanyu.03samal@gmail.com>
2022-04-26 06:58:12 +0530

    updated to const

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,18 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 /* GET /register listing. */
-router.get('/register', function(req, res, next) {
+router.get('/register', function (req, res, next) {
   res.render('register', { title: 'Library MS | Register' });
 });
 
 /* GET /login listing. */
-router.get('/login', function(req, res, next) {
+router.get('/login', function (req, res, next) {
   res.render('login', { title: 'Library MS | Login' });
 });
 
