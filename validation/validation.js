de2639e32a5b336ec71cabfa702770d3f7552176
Somanyu <somanyu.03samal@gmail.com>
2022-04-29 09:21:51 +0530

    Overriding the key name in Joi error messages.

--- validation/validation.js
+++ validation/validation.js
@@ -6,23 +6,28 @@ const registerValidation = (data) => {
         firstName: Joi.string()
                 .alphanum()
                 .min(4)
-                .required(),
+                .required()
+                .label("First Name"),
         lastName: Joi.string()
                 .alphanum()
                 .min(4)
-                .required(),
+                .required()
+                .label("Last Name"),
         email: Joi.string()
                 .min(10)
                 .required()
-                .email(),
+                .email()
+                .label("E-mail"),
         password: Joi.string()
                 .regex(/^[a-zA-Z0-9]{8,30}$/)
                 .min(8)
-                .required(),
+                .required()
+                .label("Password"),
         confirmPassword: Joi.string()
                 .regex(/^[a-zA-Z0-9]{8,30}$/)
                 .min(8)
                 .required()
+                .label("Confirm Password")
       });
       return schema.validate(data);
 };
@@ -36,11 +41,13 @@ const schemaLogin = Joi.object({
     email: Joi.string()
         .min(10)
         .required()
-        .email(),
+        .email()
+        .label("E-mail"),
     password: Joi.string()
         .min(8)
         .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$'))
-        .required(),
+        .required()
+        .label("Password"),
 });
 
 const loginValidation = (data) => {
@@ -52,10 +59,12 @@ const categoryValidation = (data) => {
         const schema = Joi.object({
                 categoryId: Joi.number()
                         .min(1)
-                        .required(),
+                        .required()
+                        .label("Category ID"),
                 categoryName: Joi.string()
                         .min(5)
                         .required()
+                        .label("Category Name")
           });
           return schema.validate(data);
     };
@@ -64,10 +73,12 @@ const publicationValidation = (data) => {
         const schema = Joi.object({
                 publicationId: Joi.number()
                         .min(1)
-                        .required(),
+                        .required()
+                        .label("Publication ID"),
                 publicationName: Joi.string()
                         .min(3)
                         .required()
+                        .label("Publication Name")
         });
         return schema.validate(data);
 }
