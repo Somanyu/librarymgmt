24040ebe6270ac18288671efa2e3078f08e2a99e
Somanyu <somanyu.03samal@gmail.com>
2022-04-29 09:22:35 +0530

    Added "Publication" methods

--- routes/library.js
+++ routes/library.js
@@ -1,7 +1,8 @@
 var express = require('express');
 const { requireAuth, checkUser } = require('../middleware/authMiddleware');
-const { categoryValidation } = require('../validation/validation');
+const { categoryValidation, publicationValidation } = require('../validation/validation');
 const Category = require('../model/Category');
+const Publication = require('../model/Publication');
 var router = express.Router();
 
 router.get('*', checkUser);
@@ -43,7 +44,7 @@ router.post('/category', requireAuth, async (req, res) => {
         req.flash('message', 'Book with this Category already exists.');
         res.redirect('/library/category');
     }
-     else {
+    else {
 
         const category = new Category({
             categoryId: categoryId,
@@ -65,7 +66,58 @@ router.post('/category', requireAuth, async (req, res) => {
 
 /* GET Publication details after logged in. */
 router.get('/publication', requireAuth, (req, res) => {
-    res.render('publication');
+    Publication.find(function (err, content) {
+        if (content) {
+            res.render('publications', {
+                title: 'Library | Publication',
+                contents: content,
+                message: req.flash('message')
+            });
+        } else {
+            console.log(err);
+        }
+    });
+});
+
+/* POST Publication details into MongoDB. */
+router.post('/publication', requireAuth, async (req, res) => {
+    const publicationId = req.body.publicationId;
+    const publicationName = req.body.publicationName;
+
+    // Checks for the publication validation.
+    const { error } = publicationValidation(req.body);
+    if (error) {
+        req.flash('message', error.details[0].message);
+        res.redirect('/library/publication');
+    }
+
+    // Check if publicationId and publicationName exists in DB.
+    const publicationIdExist = await Publication.findOne({ publicationId: publicationId, publicationName: publicationName });
+    const publicationNameExist = await Publication.findOne({ publicationName: publicationName });
+    if (publicationIdExist) {
+        req.flash('message', 'Book with this publication already exists.');
+        res.redirect('/library/publication');
+    } else if (publicationNameExist) {
+        req.flash('message', 'Book with this publication already exists.');
+        res.redirect('/library/publication');
+    }
+    else {
+
+        const publication = new Publication({
+            publicationId: publicationId,
+            publicationName: publicationName
+        });
+
+        try {
+            const savedPublication = await publication.save();
+            // res.send({ categoryId: category._id, categoryId: category.categoryId, categoryName: category.categoryName });
+            // res.render('profile');
+            res.redirect('/library/publication');
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
 });
 
 module.exports = router;
